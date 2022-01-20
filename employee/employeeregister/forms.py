22116f5c6fafbf160cd4e62e1b0342e0924804f3
Victor Gallegos <316160456@ciencias.unam.mx>
2022-01-19 19:50:23 -0600

    Sueldo modificacion

--- employee/employeeregister/forms.py
+++ employee/employeeregister/forms.py
@@ -11,6 +11,6 @@ class employeeForm(forms.ModelForm):
         widgets = {
             'FullName': forms.TextInput(attrs={'class': 'form-control', 'id': 'fullName', 'placeholder': 'Nombre Completo'}),
             'mobile_no': forms.TextInput(attrs={'class': 'form-control', 'id': 'mobNo', 'placeholder': '# de trabajador'}),
-            'emp_code': forms.TextInput(attrs={'class': 'form-control', 'id': 'emp_code', 'placeholder': 'Emp. Code'}),
+            'emp_code': forms.TextInput(attrs={'class': 'form-control', 'id': 'emp_code', 'placeholder': '3000 ejemplo'}),
             'position': forms.Select(attrs={'class': 'form-control', 'id': 'exampleFormControlSelect1'}),
         }
