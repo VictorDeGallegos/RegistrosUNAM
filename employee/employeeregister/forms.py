dfcd1e99230abe3596aca58c2b5726072fbca691
Victor Gallegos <316160456@ciencias.unam.mx>
2022-01-20 19:41:38 -0600

    Modificando forma de llenar forms

--- employee/employeeregister/forms.py
+++ employee/employeeregister/forms.py
@@ -10,12 +10,12 @@ class employeeForm(forms.ModelForm):
         fields = ("nombre_completo", "CURP", "direccion",
                   "numero_de_empleado", "años_de_antiguedad", "email", "sueldo", "position")
         widgets = {
-            'nombre_completo': forms.TextInput(attrs={'class': 'form-control', 'id': 'nombre_completo', 'placeholder': 'Nombre Completo'}),
-            'CURP': forms.TextInput(attrs={'class': 'form-control', 'id': 'CURP', 'placeholder': 'CURP'}),
-            'direccion': forms.TextInput(attrs={'class': 'form-control', 'id': 'direccion', 'placeholder': 'Direccion'}),
-            'numero_de_empleado': forms.TextInput(attrs={'class': 'form-control', 'id': 'mobNo', 'placeholder': 'No. de empleado'}),
-            'años_de_antiguedad': forms.TextInput(attrs={'class': 'form-control', 'id': 'años_de_antiguedad', 'placeholder': 'Antiguedad'}),
-            'email': forms.TextInput(attrs={'class': 'form-control', 'id': 'email', 'placeholder': 'email'}),
-            'sueldo': forms.TextInput(attrs={'class': 'form-control', 'id': 'sueldo', 'placeholder': '3000 ejemplo'}),
+            'nombre_completo': forms.TextInput(attrs={'class': 'form-control', 'id': 'nombre_completo', 'placeholder': 'Ejemplo: Victor Hugo Gallegos Mota'}),
+            'CURP': forms.TextInput(attrs={'class': 'form-control', 'id': 'CURP', 'placeholder': 'Ejemplo: GAMV991024HDFLTC01'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control', 'id': 'direccion', 'placeholder': 'Ejemplo: Investigación Científica, C.U., Coyoacán, 04510 Ciudad de México, CDMX'}),
+            'numero_de_empleado': forms.TextInput(attrs={'class': 'form-control', 'id': 'mobNo', 'placeholder': 'Ejemplo: 316160456'}),
+            'años_de_antiguedad': forms.TextInput(attrs={'class': 'form-control', 'id': 'años_de_antiguedad', 'placeholder': 'Ejemplo: 3'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'id': 'email', 'placeholder': 'Ejemplo: admin@ciencias.unam.mx'}),
+            'sueldo': forms.TextInput(attrs={'class': 'form-control', 'id': 'sueldo', 'placeholder': 'Ejemplo: 3000'}),
             'position': forms.Select(attrs={'class': 'form-control', 'id': 'exampleFormControlSelect1'}),
         }
