182fa13b4590a4c23f9fd988d273814eb20810b8
Victor Gallegos <316160456@ciencias.unam.mx>
2022-01-20 19:20:16 -0600

    Detallando curp

--- employee/employeeregister/migrations/0001_initial.py
+++ employee/employeeregister/migrations/0001_initial.py
@@ -28,7 +28,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True,
                  primary_key=True, serialize=False, verbose_name='ID')),
                 ('nombre_completo', models.CharField(max_length=100)),
-                ('CURP', models.CharField(max_length=18)),
+                ('CURP', models.CharField(max_length=18, validators=[RegexValidator(
+                    r'^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$')])),
                 ('direccion', models.CharField(max_length=200)),
                 ('sueldo', models.CharField(max_length=5,
                  validators=[RegexValidator(r'^\d{1,10}$')])),
--- employee/employeeregister/models.py
+++ employee/employeeregister/models.py
@@ -14,8 +14,9 @@ class position(models.Model):
 
 class employee(models.Model):
     nombre_completo = models.CharField(max_length=100)
-    CURP = models.CharField(max_length=18)
-    direccion = models.CharField(max_length=200)
+    CURP = models.CharField(max_length=18, validators=[
+        RegexValidator(r'^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$')])
+    direccion = models.CharField(max_length=200, )
     sueldo = models.CharField(max_length=5, validators=[
         RegexValidator(r'^\d{1,10}$')])
     numero_de_empleado = models.CharField(max_length=10, validators=[
