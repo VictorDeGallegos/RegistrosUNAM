94d70db9261e7db5cd324593be88346d556c403d
Victor Gallegos <316160456@ciencias.unam.mx>
2022-03-07 21:13:47 -0600

    Agregando static root

--- employee/employee/settings.py
+++ employee/employee/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(Base_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
